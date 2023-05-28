fdf6197040a7d399e92acaa4a5ed2af56711626d
dblatcher <dv.bltchr@gmail.com>
2023-05-28 22:55:49 +0100

    Porivder attempts to enable the sound deck on first user click

--- packages/package-demo-app/src/components/SoundDeckProvider.tsx
+++ packages/package-demo-app/src/components/SoundDeckProvider.tsx
@@ -4,9 +4,24 @@ import { SoundDeck } from 'sound-deck'
 const SoundDeckContext = createContext(new SoundDeck())
 
 export const SoundDeckProvider = ({ value, children }: { value?: SoundDeck, children: ReactNode }) => {
-    return <SoundDeckContext.Provider value={value || new SoundDeck()} >
-        {children}
-    </SoundDeckContext.Provider>
+
+    const soundDeck = value || new SoundDeck()
+
+    if (window) {
+        if (!soundDeck.isEnabled) {
+            window.addEventListener('click', () => {
+                soundDeck.enable().then(() => {
+                    console.log('enabled')
+                })
+            }, { once: true })
+        }
+    }
+
+    return (
+        <SoundDeckContext.Provider value={value || new SoundDeck()} >
+            {children}
+        </SoundDeckContext.Provider>
+    )
 }
 
 export const useSoundDeck = () => {
